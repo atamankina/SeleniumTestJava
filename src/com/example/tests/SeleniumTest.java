b9000baae1cbaffe9b19ad8a65b0ecbd8876ea93
Galina Atamankina <g.atamankina@win.tu-berlin.de>
2017-06-05 19:22:39 +0200

    Improve SeleniumTest class

--- src/com/example/tests/SeleniumTest.java
+++ src/com/example/tests/SeleniumTest.java
@@ -19,21 +19,18 @@ public class SeleniumTest {
 	public static WebDriver driver;
 
 	/**
+	 * set up the test environment using the Chrome browser
 	 * @param chromepath = path to the chrome driver on the machine
+	 * @param url = url of the website
 	 */
-	public void setChromeEnvironment(String chromepath){
+	public void setChromeEnvironment(String chromepath, String url){
 		System.setProperty("webdriver.chrome.driver", chromepath);
-		startChromeDriver();
-	}
-	
-	/**
-	 * sets the static driver variable to chrome driver
-	 */
-	private void startChromeDriver(){
 		driver = new ChromeDriver();
+		driver.get(url);
 	}
-	
+			
 	/**
+	 * find and click specific element by xpath
 	 * @param xpath = path to the required element
 	 * @throws InterruptedException because of the thread sleep to slow down the process
 	 */
@@ -44,20 +41,31 @@ public class SeleniumTest {
 	}
 	
 	/**
+	 * verify if image is shown on the page by image xpath
 	 * @param xpath = path to the required image
 	 * @throws InterruptedException because of the thread sleep to slow down the process
 	 */
 	public void verifyImagePresentByXpath(String xpath) throws InterruptedException{
 		WebElement image = driver.findElement(By.xpath(xpath));
 		Thread.sleep(2000);
-    	Boolean imagePresent = (Boolean) ((JavascriptExecutor)driver).executeScript("return arguments[0].complete && typeof arguments[0].naturalWidth != \"undefined\" && arguments[0].naturalWidth > 0", image);
+    	Boolean imagePresent = (Boolean) ((JavascriptExecutor)driver)
+    			.executeScript("return arguments[0].complete && "
+    					+ "typeof arguments[0].naturalWidth != \"undefined\" && "
+    					+ "arguments[0].naturalWidth > 0", image);
 		if(imagePresent){
 			Actions action = new Actions(driver);
 			action.moveToElement(image).perform();
-			System.out.println("Image is shown with the size" + image.getSize());
+			System.out.println("Image is shown with the size " + image.getSize());
 		}
-		else{System.out.println("Image is not shown.");}
+		else{System.out.println("Image is not shown");}
 
+	}
+	
+	/**
+	 * shut down the environment after test
+	 */
+	public void shutDownDriver(){
+		if (driver != null) driver.quit();
 	}
 	
 	
