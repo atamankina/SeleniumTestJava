11e02cee72d743e7d66dab55c40b8acff5fd3b5e
Galina Atamankina <g.atamankina@win.tu-berlin.de>
2017-06-05 18:16:31 +0200

    Added comments, renamed to main

--- src/com/example/tests/Main.java
+++ src/com/example/tests/Main.java
@@ -4,6 +4,7 @@
 package com.example.tests;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -13,7 +14,7 @@ import org.openqa.selenium.interactions.Actions;
  * @author Galina
  *
  */
-public class SelemiumTest {
+public class Main {
 
 	/**
 	 * @param args
@@ -21,32 +22,46 @@ public class SelemiumTest {
 	 */
 	public static void main(String[] args) throws InterruptedException {
 		 
-		  String chromepath = "./chromedriver/chromedriver.exe";
-		  		 
+		  //set environment
+		  String chromepath = "./chromedriver/chromedriver.exe";	 
 		  System.setProperty("webdriver.chrome.driver", chromepath);
 
+		  //go to website
 		  WebDriver driver = new ChromeDriver();
 		  driver.get("https://developer.here.com/");
 		  Thread.sleep(1000); //Thread sleep is for a user to see the progress.
+		  
+		  //find and click
 		  WebElement topmenu = driver.findElement(By.xpath("//*[@id=\"container\"]/header/div/div[2]/nav/ul/li[1]/div/span")); 
           Thread.sleep(1000);
 		  topmenu.click();
 		  Thread.sleep(1000);
+		  
+		  //find and click
 		  WebElement dropdown = driver.findElement(By.xpath("//*[@id=\"container\"]/header/div/div[2]/nav/ul/li[1]/div/div/ul/ul[4]/li[4]/a"));
 		  Thread.sleep(1000);
 		  dropdown.click();
+		  
+		  //find and click
 		  WebElement sidemenu = driver.findElement(By.xpath("//*[@id=\"toc\"]/ul/li[1]/ul/li[1]/a"));
 		  Thread.sleep(1000);
 		  sidemenu.click();
+		  
+		  //verify image
 		  WebElement image = driver.findElement(By.xpath("//*[@id=\"whatis__fig-whatis-example\"]/a/img"));
 		  Thread.sleep(2000);
 		  Actions action = new Actions(driver);
 		  action.moveToElement(image);
 		  action.perform();
 		  System.out.println(image.getSize());
+		  Boolean imagePresent = (Boolean) ((JavascriptExecutor)driver).executeScript("return arguments[0].complete && typeof arguments[0].naturalWidth != \"undefined\" && arguments[0].naturalWidth > 0", image);
+		  String result = imagePresent ? "Test successful." : "Test failed." ;
+		  System.out.println(result);
+		  
+		  //quit
 		  Thread.sleep(5000);
 		  driver.quit();
-
+		  
 
 	}
 
